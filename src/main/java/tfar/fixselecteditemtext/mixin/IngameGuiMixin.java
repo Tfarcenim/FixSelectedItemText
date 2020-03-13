bc71d82816803b3f2a7b2e600e3d20c52fb301b2
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2020-03-13 15:19:47 -0400

    Initial commit

---
+++ src/main/java/tfar/fixselecteditemtext/mixin/IngameGuiMixin.java
@@ -0,0 +1,20 @@
+package tfar.fixselecteditemtext.mixin;
+
+import net.minecraft.client.gui.IngameGui;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import tfar.fixselecteditemtext.FixSelectedItemText;
+
+@Mixin(IngameGui.class)
+public class IngameGuiMixin {
+	@Shadow protected int scaledHeight;
+
+	@ModifyVariable(method = "renderSelectedItem",
+					at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/PlayerController;shouldDrawHUD()Z")
+					,ordinal = 1)
+	private int fixOffset(int old) {
+		return FixSelectedItemText.fix(old,scaledHeight);
+	}
+}
